8bf589dc30a576317d9dd5b81d9f364a7aa162c9
Phelipe Vieira <contato@phvieira.com>
2022-03-08 10:21:20 -0300

    Ajustando projeto para analogia a formigueiro

--- PhelipeVieira.Teste.Performance/Program.cs
+++ PhelipeVieira.Teste.Performance/Program.cs
@@ -1,87 +1,94 @@
 ﻿using System.Diagnostics;
 
-var ciclos = new List<int>();
+
+var formigasDoFormigueiro = new List<int>();
+
+
 for (int i = 0; i < 10; i++)
 {
-    ciclos.Add(i);
+    formigasDoFormigueiro.Add(i);
 }
 var watcher = new Stopwatch();
 var resultados = new List<(long ElapsedMilisenconds, string Cenario)>();
 
+
 #region Execução sequencial e síncrona
 watcher.Start();
-foreach (var ciclo in ciclos)
+foreach (var formiga in formigasDoFormigueiro)
 {
-    ExecutarDeFormaSincrona(ciclo);
+    BuscarAcucarDeFormaProceduralSincrona(formiga);
 }
 watcher.Stop();
-resultados.Add((watcher.ElapsedMilliseconds,"Execução sequencial e síncrona"));
+resultados.Add((watcher.ElapsedMilliseconds, "Execução procedural e síncrona"));
 watcher.Reset();
 #endregion
 
+
 #region Execução paralela e síncrona
 watcher.Start();
-Parallel.ForEach(ciclos, x => ExecutarDeFormaSincronaParallel(x));
+Parallel.ForEach(formigasDoFormigueiro, x => BuscarAcucarDeFormaParalelizadaSincrona(x));
 watcher.Stop();
 resultados.Add((watcher.ElapsedMilliseconds, "Execução paralela e síncrona"));
 watcher.Reset();
 #endregion
 
-#region Execução sequencial e assíncrona
+
+#region Execução procedural e assíncrona
 watcher.Start();
-var tasks = new List<Task>();
-foreach (var ciclo in ciclos)
+var atividadesFormigasProceduralAssincrona = new List<Task>();
+foreach (var formiga in formigasDoFormigueiro)
 {
-    tasks.Add(ExecutarDeFormaAsincrona(ciclo));
+    atividadesFormigasProceduralAssincrona.Add(BuscarAcucarDeFormaProceduralAssincrona(formiga));
 }
-await Task.WhenAll(tasks);
+await Task.WhenAll(atividadesFormigasProceduralAssincrona);
 watcher.Stop();
-resultados.Add((watcher.ElapsedMilliseconds, "Execução sequencial e assíncrona"));
+resultados.Add((watcher.ElapsedMilliseconds, "Execução procedural e assíncrona"));
 watcher.Reset();
 #endregion
 
+
 #region Execução paralela e assíncrona
 watcher.Start();
-var tasks2 = new List<Task>();
-Parallel.ForEach(ciclos, x => tasks2.Add(ExecutarDeFormaAsincronaParallel(x)));
-await Task.WhenAll(tasks2);
+var atividadesFormigasParalelizadaAssincrona = new List<Task>();
+Parallel.ForEach(formigasDoFormigueiro, x => atividadesFormigasParalelizadaAssincrona.Add(BuscarAcucarDeFormaParalelizadaAssincrona(x)));
+await Task.WhenAll(atividadesFormigasParalelizadaAssincrona);
 watcher.Stop();
 resultados.Add((watcher.ElapsedMilliseconds, "Execução paralela e assíncrona"));
 watcher.Reset();
 #endregion
+
 
 foreach (var resultado in resultados)
 {
     Console.WriteLine($"Tempo decorrido em milisegundos: {resultado.ElapsedMilisenconds} | Cenário: {resultado.Cenario}");
 }
 
+
 Console.ReadKey();
 
-#region Comandos
-void ExecutarDeFormaSincrona(int ciclo)
-{
-    Thread.Sleep(537);
-    Console.WriteLine(" Executando de forma sincrona sequencial");
-}
-void ExecutarDeFormaSincronaParallel(int ciclo)
-{
-    Thread.Sleep(537);
-    Console.WriteLine(" Executando de forma sincrona com Parallel.ForEach");
-}
-// Desabilitei os warnings, pois o await é feito no Task.WhenAll() e o VS não reconhece, segue leitura para melhor esclarecimento: https://nelsonparente.medium.com/task-whenall-in-a-nutshell-1379428a1a53
-#pragma warning disable CS1998 
-async Task ExecutarDeFormaAsincrona(int ciclo)
-{
-    Thread.Sleep(537);
-    Console.WriteLine(" Executando de forma assincrona pilha de tasks");
-}
-#pragma warning restore CS1998 
 
-#pragma warning disable CS1998 
-async Task ExecutarDeFormaAsincronaParallel(int ciclo)
+#region Comandos
+void BuscarAcucarDeFormaProceduralSincrona(int formiga)
 {
     Thread.Sleep(537);
-    Console.WriteLine(" Executando de forma assincrona paralela");
+    Console.WriteLine(" Executando de forma procedural síncrona");
 }
-#pragma warning restore CS1998 
-#endregion
+
+void BuscarAcucarDeFormaParalelizadaSincrona(int formiga)
+{
+    Thread.Sleep(537);
+    Console.WriteLine(" Executando de forma paralelizada e síncrona");
+}
+
+async Task BuscarAcucarDeFormaProceduralAssincrona(int formiga)
+{
+    Thread.Sleep(537);
+    Console.WriteLine(" Executando de forma procedural e assíncrona");
+}
+
+async Task BuscarAcucarDeFormaParalelizadaAssincrona(int formiga)
+{
+    Thread.Sleep(537);
+    Console.WriteLine(" Executando de forma paralela e assíncrona");
+}
+#endregion;
